93a7f93270a10d6ff26dd8d7107f6f4b4eb9bb8a
mosmant <mottnr@gmail.com>
2021-10-11 19:55:01 +0300

    11.10.2021

---
+++ src/test/java/day06/C99_Mentor_Practise.java
@@ -0,0 +1,27 @@
+package day06;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class C99_Mentor_Practise {
+
+    //Navigate to "https://react-shopping-cart-67954.firebaseapp.com/"
+    // List all the elements on the website
+    // Create a String Arraylist and put Product Names into the ArrayList
+    // Choose 5 items randomly, add to cart and print the names of items
+    // (Each product cannot be added more then 1 time!)
+    // CHECK THE PRICES!
+    // Sum each item's price and compare your result with the website
+    // If results are matching print success message
+    // Get string price values from Website, convert them to double and add to <Double> ArrayList and print the price list
+    // Click Checkout
+    @BeforeClass
+    public static void setup(){}
+    @Test
+    public void test(){}
+    @AfterClass
+    public static void tearDown(){}
+
+
+}
